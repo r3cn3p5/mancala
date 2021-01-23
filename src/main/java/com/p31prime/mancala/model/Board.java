fd80a3817c784deec1a4496ad3f1d012231a1e0e
spencer <spencer@31prime.co.uk>
2021-01-23 10:38:38 +0000

    Completed rules

--- src/main/java/com/p31prime/mancala/model/Board.java
+++ src/main/java/com/p31prime/mancala/model/Board.java
@@ -9,7 +9,9 @@ public class Board {
             sides[s] = new BoardSide();
     }
 
-    public void play(int player, int pocket) {
+    public boolean play(int player, int pocket) {
+
+        boolean replay = false;
 
         // player can only by 0 or 1
         int side = player;
@@ -20,60 +22,74 @@ public class Board {
         sides[side].pockets[pocket] = 0;
         pocket++;
 
-        // ok we have some stones lets keep moving
-
         while (stones > 0) {
 
             if (pocket >= 6) {
 
                 // drop into store if players side
-                if (side == player)
+                if (side == player) {
                     sides[side].store++;
+                    replay = stones == 1 ? true : false;
+                }
 
                 stones--;
                 pocket = 0;
 
                 // switch sides and continue
                 side = side==0 ? 1: 0;
-
                 continue;
             }
 
+            // if last stone on player pocket take other side.
             if (side == player && stones == 1 && sides[side].pockets[pocket] == 0) {
                 sides[side].store++;
                 sides[side].pockets[pocket] = 0;
 
                 sides[side].store = sides[side==0 ? 1: 0].pockets[pocket];
                 sides[side==0 ? 1: 0].pockets[pocket] = 0;
-
-            } else {
-                sides[side].pockets[pocket]++;
+                break;
             }
 
+            // standard old go.
+            sides[side].pockets[pocket]++;
             stones--;
             pocket++;
 
         }
 
-    }
+        // end game
+        for (int s=0; s<2; s++ ) {
 
-    public boolean detectEndGame() {
+            int t=0;
+            for (int p=0; p<6; p++)
+                t+=sides[s].pockets[p];
 
-        return false;
+            if (t==0) {
+                for (int p=0; p<6; p++) {
+                    t += sides[s == 0 ? 1 : 0].pockets[p];
+                    sides[s == 0 ? 1 : 0].pockets[p] = 0;
+                }
+                sides[s==0 ? 1: 0].store += t;
+                break;
+            }
+        }
+
+        return replay;
     }
 
     public void printBoard() {
 
-        for (int s=0; s<2; s++) {
+        System.out.format("S(%2d) ",sides[0].store);
+        for (int p=5; p >= 0; p--)
+            System.out.format("(%2d) ",sides[0].pockets[p]);
+        System.out.println();
 
-            System.out.print("S(" + sides[s].store + ") ");
+        System.out.print("      ");
+        for (int p=0; p < 6; p++)
+            System.out.format("(%2d) ",sides[1].pockets[p]);
+        System.out.format("S(%2d)",sides[1].store);
+        System.out.println();
 
-            for (int p=0; p < 6; p++)
-                System.out.print("P"+p+"("+sides[s].pockets[p]+")");
-
-            System.out.println();
-
-        }
     }
 
 }
